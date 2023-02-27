bcdba7a18b000c6fe8afeb427654debd1e47f60f
Nadya <Nadi-lv@yandex.ru>
2023-02-27 12:49:25 +0400

    metod SecondArray

---
+++ ItogZadacha/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] array1 = new string [5] {"1234", "1256", "-2", "world", ":-)"};
+string[] array2 = new string [array1.Length];
+
+void SecondArray (string[] array1, string[] array2)
+{
+    int count = 0;
+    for (i = 0; i <= array1.Length; i++)
+    {
+        if (array1[i]<=3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
